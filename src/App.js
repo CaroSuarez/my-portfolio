09f430c7ef586b69a9a866df105d197a505c7b52
carosuarez <cslondo90@gmai.com>
2021-01-18 19:09:08 -0500

    (Config:) Files Structure Set

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import AboutMe from './scenes/aboutMe/AboutMe'
+import Contact from './scenes/contact/Contact'
+import Home from './scenes/home/Home'
+import Projects from './scenes/projects/Project'
 
 
 function App() {
   return (
     <Router>
       <Switch>
-          <Route path="/aboutMe" component= {aboutMe}/>
-          <Route path="/projects" component = {projects} />
-          <Route path="/contact" component = {contact} />
-          <Route path="/" component = {home} />
-          <Route component={NotFound} />
+          <Route path="/aboutMe" component= {AboutMe}/>
+          <Route path="/projects" component = {Projects} />
+          <Route path="/contact" component = {Contact} />
+          <Route path="/" component = {Home} />
+          {/* <Route component={NotFound} /> */}
         </Switch>
     </Router>
    
