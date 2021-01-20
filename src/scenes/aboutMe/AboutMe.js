a4c9534fe4e554e3bbc5a025a63acefeebfee4fb
carosuarez <cslondo90@gmai.com>
2021-01-19 19:01:24 -0500

    (Home:) welcome text and background ready

--- src/scenes/aboutMe/AboutMe.js
+++ src/scenes/aboutMe/AboutMe.js
@@ -1,9 +1,16 @@
-import React from 'react';
-
+import React, { Fragment } from 'react';
+import './aboutMe.scss'
 
 const AboutMe = () => {
     return (
-        <div>About Me</div>
+
+        <Fragment>
+            <div id = 'welcomeTittle'>
+                <div>Welcome! </div> 
+                <div>I'm  <em>C</em>aro<em>S</em>uárez  </div>
+                <div id = 'frontEnd' >Front-End Developer</div>
+            </div>
+        </Fragment>
     )    
 }
 
